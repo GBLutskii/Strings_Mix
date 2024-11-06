f8d68aceb922fa7638baa1ccb788c75610866cfb
Gabriel (Egor Guschin) Lutskii <115532859+GBLutskii@users.noreply.github.com>
2024-11-06 18:09:25 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars_Strings_Mix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = Console.ReadLine();
+            string s2 = Console.ReadLine();
+
+            Console.WriteLine(Mixing.Mix(s1, s2));
+
+            Console.ReadKey();
+        }
+    }
+}
